4ca4e11ad14e1aa85a849a23423a2d69a2e24d7b
doctorbond2 <michael.rostlund@hotmail.com>
2025-04-11 08:56:47 +0200

    add Module class with initialization and destruction methods

---
+++ src/module/modue.ts
@@ -0,0 +1,14 @@
+
+export class Module {
+  constructor() {
+    console.log("Module initialized");
+  }
+
+  init() {
+    console.log("Module initialized");
+  }
+
+  destroy() {
+    console.log("Module destroyed");
+  }
+}
