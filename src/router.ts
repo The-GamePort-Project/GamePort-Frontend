3ff7d9682f07731aa7b62ae506eec548e0b5ca32
doctorbond2 <michael.rostlund@hotmail.com>
2025-04-23 20:14:25 +0200

    fixed importts for router.

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
+import LoginPage from "./features/auth/pages/LoginPage";
 import ErrorPage from "./pages/ErrorPage";
 import App from "./App";
 import React from "react";
